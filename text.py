7f3701e74bb1a14d13a5755939a6e3d8da1c2e55
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-19 00:41:30 +0100

    Update text.py

--- text.py
+++ text.py
@@ -1,15 +1,14 @@
 class script(object):
     START_MSG = """Hi {},
     
-    This is a simple bot to forward all messages from one channel to other"""
+    i am a simple bot than can automatically forward messages from one channel to other"""
 
-    HELP_MSG = """♻️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒: <a href=https://telegram.me/Rokubotz>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓"""
+    HELP_MSG = """<a href=https://telegram.me/MysterySD>꧁❏รเlєภt ๔є๓๏ภ❏꧂</a>"""
 
     ABOUT_TXT = """
 📚 𝖫𝗂𝖻𝗋𝖺𝗋𝗒: <a href=https://github.com/pyrogram>𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆</a>
 🚀 𝖲𝖾𝗋𝗏𝖾𝗋 : <a href=https://www.heroku.com>𝖧𝖾𝗋𝗈𝗄𝗎</a>
 📝 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾: <a href=https://www.python.org>𝖯𝗒𝗍𝗁𝗈𝗇3</a>
 💾 𝖣𝖺𝗍𝖺𝖡𝖺𝗌𝖾: <a href=https://cloud.mongodb.com>𝖬𝗈𝗇𝗀𝗈𝖣𝖡</a>
-🧑‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋: <a href=https://telegram.me/MysterySD>꧁❏รเlєภt ๔є๓๏ภ❏꧂</a>
 
 ♻️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒: <a href=https://telegram.me/Rokubotz>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓</a> </b>"""
