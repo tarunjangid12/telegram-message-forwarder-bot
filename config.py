b1af5571b379351302a69289bb554a8cfac85b0a
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-18 16:45:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config:
 
+
     # Database Config 
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority") # ⚠️ Required
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Test")
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
+    
