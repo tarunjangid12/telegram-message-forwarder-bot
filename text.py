14f64162947a87151a476e344e9bf83ec31b74cb
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-19 00:36:49 +0100

    Update text.py

--- text.py
+++ text.py
@@ -1,40 +1,13 @@
 class script(object):
-    START_MSG = """Hi {},
-This is a simple bot to forward all messages from one channel to other
+    START_MSG = """Hi {},This is a simple bot to forward all messages from one channel to other"""
 
-⚠️Warning
-Your account may get banned if you forward more files(from private channels). Use at Own Risk!!"""
+    HELP_MSG = """♻️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒: <a href=https://telegram.me/Rokubotz>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓"""
 
-    HELP_MSG = """Available commands:-
-➜ /index - To index a channel
-➜ /forward - To start forwarding
-➜ /total - Count total messages in DB
-➜ /status - Check Current status
-➜ /help - Help data
-➜ /stop - To stop all running processes. 
+    ABOUT_TXT = """
+📚 𝖫𝗂𝖻𝗋𝖺𝗋𝗒: <a href=https://github.com/pyrogram>𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆</a>
+🚀 𝖲𝖾𝗋𝗏𝖾𝗋 : <a href=https://www.heroku.com>𝖧𝖾𝗋𝗈𝗄𝗎</a>
+📝 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾: <a href=https://www.python.org>𝖯𝗒𝗍𝗁𝗈𝗇3</a>
+💾 𝖣𝖺𝗍𝖺𝖡𝖺𝗌𝖾: <a href=https://cloud.mongodb.com>𝖬𝗈𝗇𝗀𝗈𝖣𝖡</a>
+🧑‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋: <a href=https://telegram.me/MysterySD>꧁❏รเlєภt ๔є๓๏ภ❏꧂</a>
 
-Use /index to index messages from a channel to database.
-
-After indexing you can start forwarding by using /forward.
-
-<b>⦿ Note:</b>
-You will require the following data to index a channel:-
-
-☛ <b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.
-
-☛ <b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @MissRose_bot.
-
-☛ <b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting
-
-☛ <b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions.
-After fowarding completes use the /cleardb command to clean your database."""
-
-    ABOUT_TXT = """<b>╭───────────⍟
-├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/Snowball_Official>Sɴᴏᴡʙᴀʟʟ</a>
-├👑 Instagram : <a href=https://www.instagram.com/ritesh6_>C-Insta</a> 
-├☃️ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/+HzGpLAZXTxoyYTNl>Rᴏᴏғɪᴠᴇʀsᴇ</a>
-├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
-├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
-├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
-├🌀 ᴍʏ ꜱᴇʀᴠᴇʀ : <a href=https://dashboard.heroku.com>Heroku</a>
-╰───────────────⍟ """
+♻️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒: <a href=https://telegram.me/Rokubotz>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓</a> </b>"""
